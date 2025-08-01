1755a3b4b400d32ed6ce2b004a4e5ec4abed0916
Etan Petel <130949411+EtanPtl@users.noreply.github.com>
2025-08-01 14:43:19 -0400

    Creating routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-
 import './App.css'
-
+import { Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home"
+import SignUp from "./pages/SignUp"
+import Queue from "./pages/Queue"
 function App() {
-
+  // Creating Routes 
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path="/" element={<Home/>}>
+        <Route path="/SignUp" index element={<SignUp/>} />
+        <Route path="/Queue" index element={<Queue/>}/>
+
+      </Route>
+    </Routes>
   )
 }
 
