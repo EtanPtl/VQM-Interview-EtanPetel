20758cf7e310522fff1ebdab32d3f39e74a9be1c
Etan Petel <130949411+EtanPtl@users.noreply.github.com>
2025-08-01 13:00:57 -0400

    Creating more backend endpoints
    - Created Post for queue database table (join queue)
    - Create Get to retrieve entries from queue

--- server/server.js
+++ server/server.js
@@ -20,8 +20,24 @@ app.use("/auth", auth);
 app.post("/queue", async(req, res) => {
     const { username } = req.session.user
     try {
-        const newEntry = await pool.query("INSERT INTO queue (username) VALUE($1)", [username])
+        const newEntry = await pool.query("INSERT INTO queue (username) VALUE($1)", [username]);
+        res.json(newEntry);
     } catch (error) {
         console.log(error.message);
     }
+})
+
+app.get("/queue", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const entry = await pool.query("SELECT * FROM queue WHERE queue_id = $1", [id]);
+        res.json(entry)
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+app.get("/all-queue", async(req, res) => {
+    const allEntries = await pool.query("SELECT * FROM queue");
+    res.json(allEntries.rows);
 })
