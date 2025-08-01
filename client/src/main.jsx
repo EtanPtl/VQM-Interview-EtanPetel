4e0427c0723903eefd15f2cb92caecca44f3d5a6
Etan Petel <130949411+EtanPtl@users.noreply.github.com>
2025-08-01 13:52:21 -0400

    Starting front end work
    - Use browser router so user can go to other pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";;
 import './index.css'
 import App from './App.jsx'
 
+// User browser router so that user can go to other pages
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
