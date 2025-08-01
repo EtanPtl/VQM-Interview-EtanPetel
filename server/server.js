eb3824e9afb51d7a6a7a95a7e45a593b8fa1da5e
Etan Petel <130949411+EtanPtl@users.noreply.github.com>
2025-08-01 12:55:16 -0400

    Backend setup
    - creating post endpoint and logic for express sessions

--- server/server.js
+++ server/server.js
@@ -14,3 +14,14 @@ app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
 }));
+
+app.use("/auth", auth);
+
+app.post("/queue", async(req, res) => {
+    const { username } = req.session.user
+    try {
+        const newEntry = await pool.query("INSERT INTO queue (username) VALUE($1)", [username])
+    } catch (error) {
+        console.log(error.message);
+    }
+})
